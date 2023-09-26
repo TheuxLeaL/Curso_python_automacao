4224515dfcda71fd76a9fdd5aeee49479fb1b750
Matheus Leal <matheuslealazevedo@gmail.com>
2023-09-26 17:01:08 -0300

    3

---
+++ metodos_strings.py
@@ -0,0 +1,13 @@
+# Métodos comuns de string
+nome_curso = ' Edição de Vídeo '
+print(nome_curso.upper())
+print(nome_curso.lower())
+print(nome_curso.strip()) #remove espaços
+print(nome_curso.lstrip()) #remove apenas o espaço da esquerda
+print(nome_curso.rstrip()) #remove apenas o espaço da direita
+print(nome_curso.find('ção')) #retorna o indice (posição de algo dentro de uma sequencia)
+print(nome_curso.replace('Vídeo' , 'Música')) #substitui palavras
+print('https://sc.olx.com.br/?o=90&q=relogio'.replace('relogio','carro')) #substitui a pesquisa de relogio por carro
+
+
+
