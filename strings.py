fbc8dbce038e1217061d97689589ce03517b493d
Matheus Leal <matheuslealazevedo@gmail.com>
2023-09-26 16:45:45 -0300

    2

--- strings.py
+++ strings.py
@@ -1,2 +1,18 @@
 # Strings
 nome_do_instrumento = 'Guitarra elétrica'
+nome_do_instrumento = "Guitarra elétrica"
+print(nome_do_instrumento)
+# Caracteres de Escape
+resenha_do_filme = "O filme foi \"demais!\""
+resenha_do_filme2 = "O filme foi \'demais!\'"
+resenha_do_filme3 = "O filme foi \ndemais!"
+minhas_fotos = 'meu computador\\fotos\\minhas fotos'
+print(resenha_do_filme)
+print(resenha_do_filme2)
+print(resenha_do_filme3)
+print(minhas_fotos)
+#comentario
+''' Comentario mais de uma linha
+suave
+sem problemas
+oi '''
