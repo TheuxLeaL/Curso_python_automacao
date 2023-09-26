9ab06fd2b937ef8c021ec3e390ca86af32af4e05
Matheus Leal <matheuslealazevedo@gmail.com>
2023-09-26 17:19:58 -0300

    4

---
+++ acessando_partes_string.py
@@ -0,0 +1,10 @@
+# Acessando partes de uma string
+'Mouse'
+mouse = 'Mouse'
+print(mouse[3]) #indice para acessar a letra
+descricao_do_computador = 'Esse é uma computador que foi lançado e chegou para revolucionar o mercado atual'
+print(descricao_do_computador[-1]) #extrai o ultimo caractere
+print(descricao_do_computador.index('o'))
+
+## DESAFIO encontrar indice da letra A dentro de de "uma"
+print(descricao_do_computador.index('a'))
---
+++ separando_partes_string.py
@@ -0,0 +1,14 @@
+# Exemplo facebook.com/devaprender
+link = "facebook.com/devaprender"
+print(link[0]) #primeiro caractere
+print(link[-1]) #ultimo
+print(link[0:5]) #iniciando no 0 e finalizando no 5
+print(link[0:]) #a partir de 0
+print(link[-5:]) #5 posições antes do final (voltam 5 do final)
+print(link[5:]) #inicia no indice 5 e ir até o final
+print(link[:-3]) #inicio até 3 antes do final
+
+link2 = 'instagram/devaprender'
+# DESAFIO extrair somente devaprender do link
+print(link2[-11:]) 
+print(link2[10:])
