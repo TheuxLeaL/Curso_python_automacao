2eda26fb68984e76a989cfc62bae09bdd2700550
Matheus Leal <matheuslealazevedo@gmail.com>
2023-10-28 05:09:27 -0300

    5

---
+++ listas.py
@@ -0,0 +1,33 @@
+# Listas
+site1 = 'youtube.com'
+site2 = 'facebook.com'
+site3 = 'instagram.com'
+
+sites = ['youtube.com','facebook.com','instagram.com']
+# print(sites[1])
+
+# Adicionando na lista
+sites.append('novosite.com')
+print(sites)
+
+# Removendo na lista
+sites.remove('facebook.com')
+print(sites)
+
+# Listas dinâmicas
+
+sites = ['youtube.com','facebook.com','instagram.com', 35, True]
+
+# Lista de listas
+pessoas = [['Rafael',25], ['Amanda',19]]
+print(pessoas)
+print(pessoas[1][0])
+
+# Desafio 1
+''' Desafio 1 crie uma lista que tenha os nome de 3 objetos que você mais usa durante o dia e imprima ele na tela'''
+objetos = ['pc','celular','chinelo']
+print(objetos)
+''' Dentro de cada um coloque uma info extra, coloque o valor em reais desse objeto tambem e imprima ele na tela'''
+objetos2 = [['pc',4000.00], ['celular',2000.00],['chinelo',100.00]]
+print(objetos2)
+
---
+++ receber_dados.py
@@ -0,0 +1,7 @@
+# Recebendo dados do usuário no Python
+pais = input('Onde você mora')
+print(pais)
+quantas_paginas_buscar = int(input('Quantas paginas devemos buscar?'))
+print(type(pais))
+print(type(quantas_paginas_buscar))
+                            
