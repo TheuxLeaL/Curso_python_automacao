e18107286f63ba42b353e39100288fcc96b48f39
Matheus Leal <matheuslealazevedo@gmail.com>
2023-10-28 04:55:03 -0300

    5

---
+++ operacoes_matematicas.py
@@ -0,0 +1,34 @@
+import math
+# Numeros
+pizza = 30.50
+pedaco_pizza = 5
+
+# Multiplicação
+print(pedaco_pizza * 5)
+
+# Soma
+print(pedaco_pizza + pedaco_pizza)
+
+# Subtração
+print(pedaco_pizza - pedaco_pizza)
+
+# Divisão
+print(pizza / 2)
+
+# Divisão de inteiro
+print(10 // 6)
+
+# Módulo
+print(10 % 6)
+
+# Atalhos
+a = 10
+a = a + 5
+a += 5
+a -= 5
+a *= 5
+a /= 5
+
+# Arredondar valores
+print(round(5.7))
+print(math.ceil(2.2))
